f9548e3693ce8127be4387980cae4d85a114d35f
Takagero <OpeLLIek_o0@mail.ru>
2016-10-28 07:05:58 +0300

    Яндекс карта
    Яндекс карта с точками постаматов и описанием

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Pick Point maps v1.0</title>
+
+<script src="//api-maps.yandex.ru/2.1/?lang=ru-RU" type="text/javascript"></script>
+<script src="//yandex.st/jquery/2.2.3/jquery.min.js" type="text/javascript"></script>
+<script src="object_manager.js" type="text/javascript"></script>
+
+<style>
+html, body, #map {
+width: 100%; height: 100%; padding: 0; margin: 0;
+}
+</style>
+</head>
+<body>
+	<center>
+		<div id="map" style="width: 600px; height: 400px"></div>
+	</center>
+</body>
+</html>
